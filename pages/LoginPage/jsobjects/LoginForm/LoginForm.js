406a904b8ebf5603ff6aac99a29d032af4d6183c
justin <justin@couchsurfing.com>
2024-10-04 21:43:17 +0000

    testing auth info

--- pages/LoginPage/jsobjects/LoginForm/LoginForm.js
+++ pages/LoginPage/jsobjects/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 export default {
 	async login(params) {
 		console.log('LoginForm', params);
+		console.log('current user', appsmith.user);
 		await LoginAuth.generateAccessToken(params);
 
 		navigateTo('Dashboard');
