6a1cfa91ccde13724fc9d78fc470be8c25485f5d
justin <justin@couchsurfing.com>
2024-10-04 23:14:16 +0000

    only navigate during view stage

--- pages/AuthPage/jsobjects/CushionsIntegrator/CushionsIntegrator.js
+++ pages/AuthPage/jsobjects/CushionsIntegrator/CushionsIntegrator.js
@@ -1,37 +1,39 @@
 export default {
 	async initCushions() {
 		console.log('Initializing cushions...');
-		
+
 		const redirectTo = this.getQueryParam('redirectTo', 'Dashboard');
 		const loginAs = this.getQueryParam('loginAs', appsmith.user.email);
-		
+
 		try {
-		if (this.isAuthenticationRequired(loginAs)) {
-			await this.loginAsUser(loginAs);
-		}
-		
-		// navigateTo(redirectTo);
+			if (this.isAuthenticationRequired(loginAs)) {
+				await this.loginAsUser(loginAs);
+			}
+
+			if (appsmith.mode === 'VIEW') {
+				navigateTo(redirectTo);
+			}
 		} catch (error) {
-			console.error('Error during init', error);Ï
+			console.error('Error during init', error);
 		}
 	},
-	
+
 	isAuthenticationRequired(loginAs) {
 		return true;
 	},
-	
+
 	async loginAsUser(email, password) {
-		
+
 	},
-	
+
 	getQueryParam(param, defaultValue) {
 		const isSet = param in appsmith.URL.queryParams;
 		const value = appsmith.URL.queryParams[param];
-		
+
 		if (isSet) {
 			return value;
 		}
-		
+
 		return defaultValue;
 	}
 }
