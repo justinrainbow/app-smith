79d22df88397e19c042fdc7e2ba648c6ebb8ddc8
justin <justin@couchsurfing.com>
2024-10-04 22:55:01 +0000

    broken version

---
+++ pages/AuthPage/jsobjects/CushionsIntegrator/CushionsIntegrator.js
@@ -0,0 +1,37 @@
+export default {
+	async initCushions() {
+		console.log('Initializing cushions...');
+		
+		const redirectTo = this.getQueryParam('redirectTo', 'Dashboard');
+		const loginAs = this.getQueryParam('loginAs', appsmith.user.email);
+		
+		try {
+		if (this.isAuthenticationRequired(loginAs)) {
+			await this.loginAsUser(loginAs);
+		}
+		
+		navigateTo(redirectTo);
+		} catch (error) {
+			console.error('Error during init', error);Ï
+		}
+	},
+	
+	isAuthenticationRequired(loginAs) {
+		return true;
+	},
+	
+	async loginAsUser(email, password) {
+		
+	},
+	
+	getQueryParam(param, defaultValue) {
+		const isSet = param in appsmith.URL.queryParams;
+		const value = appsmith.URL.queryParams[param];
+		
+		if (isSet) {
+			return value;
+		}
+		
+		return defaultValue;
+	}
+}
