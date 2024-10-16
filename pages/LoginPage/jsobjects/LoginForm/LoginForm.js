8bcf33efaf2d1f5fb800bc00dc965f0b9c5ea8a7
justin <justin@couchsurfing.com>
2024-10-16 18:10:02 +0000

    removed old login page

--- pages/LoginPage/jsobjects/LoginForm/LoginForm.js
+++
@@ -1,9 +0,0 @@
-export default {
-	async login(params) {
-		console.log('LoginForm', params);
-		console.log('current user', appsmith.user);
-		await LoginAuth.generateAccessToken(params);
-
-		navigateTo('Dashboard');
-	}
-}
